18eae92c3f787a3237524bf8cf814445fc167f44
pupson <pupson13@gmail.com>
2024-02-21 00:39:50 -0800

    v3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//gsap.registerPlugin(SplitText);
+
 class Line {
   constructor(opts) {
     this.line_container = opts.line_container;
@@ -27,89 +27,14 @@ class Line {
 
     this.line_container.appendChild(this.line_elm);
 
-    this.splitting = new SplitText(this.copy_elm, { type: "words,chars" });
-    gsap.set(this.splitting.chars, { opacity: 0 });
-    gsap.set(this.copy_elm, { position: "relative", justifySelf: "start" });
-
     this.underline_elm = document.createElement("div");
     this.underline_elm.classList.add("underline");
 
     this.copy_elm.appendChild(this.underline_elm);
 
-    gsap.set(this.underline_elm, {
-      position: "absolute",
-      left: 1,
-      bottom: 1,
-      width: "100%",
-      height: "0.15vw",
-      backgroundColor: "#7af042",
-      opacity: 0
-    });
     // this.animate();
   } //build
-
-  animate() {
-    let tl = gsap
-      .timeline({ paused: false, delay: this.delay })
-      .set(this.line_elm, { display: "grid" })
-      .set(this.splitting.chars, { opacity: 0, visibility: "visible" })
-      .fromTo(
-        this.line_elm,
-        {
-          "--grad-offset-scale": 1
-        },
-        {
-          "--grad-offset-scale": -1,
-          duration: 0.5,
-          ease: "power4.inOut"
-        }
-      )
-      .fromTo(
-        this.splitting.chars,
-        { opacity: 1, backgroundColor: "#7df14a" },
-        {
-          opacity: 1,
-          backgroundColor: "transparent",
-          duration: 0.05,
-          stagger: 0.05,
-          ease: "steps(1)"
-        },
-        "-=0.12"
-      )
-      .fromTo(
-        this.splitting.chars,
-        {
-          color: "#fff",
-          textShadow:
-            "0px 0px 6px rgba(255,255,255,1), 0px 0px 15px rgba(255,255,255,1)"
-        },
-        {
-          color: "#7df14a",
-          textShadow:
-            "0px 0px 6px rgba(255,255,255,0), 0px 0px 15px rgba(255,255,255,0)",
-          duration: 0.45,
-          stagger: 0.05
-        },
-        "<"
-      )
-      .add(
-        function () {
-          this.key_sound.play();
-        }.bind(this),
-        "<"
-      )
-      .add(
-        function () {
-          this.key_sound.stop();
-        }.bind(this),
-        "-=0.25"
-      );
-    if (this.has_underline) {
-      tl.set(this.underline_elm, { opacity: 1 }, "<");
-    }
-    return tl;
-  } //animate
-} //Line
+}
 
 class LineBreak {
   constructor(opts) {
@@ -126,9 +51,7 @@ class LineBreak {
 
     this.line_container.appendChild(this.line_elm);
   }
-  animate() {
-    return gsap.timeline().set(this.line_elm, { display: "grid" });
-  }
+
 } //LineBreak
 
 class ClearScreen {
@@ -149,58 +72,8 @@ class ClearScreen {
     this.root_elm.appendChild(this.top_elm);
     this.root_elm.appendChild(this.bottom_elm);
 
-    gsap.set(this.root_elm, {
-      display: "grid",
-      position: "fixed",
-      left: 0,
-      top: 0,
-      width: "100vw",
-      height: "100vh",
-      gridTemplateColumns: "1fr",
-      gridTemplateRows: "1fr 1fr",
-      opacity: 0
-    });
-    gsap.set([this.top_elm, this.bottom_elm], {
-      backgroundColor: "#80ff10",
-      opacity: 0
-    });
   } //build
-  animate() {
-    let tl = gsap
-      .timeline()
-      .add(
-        function () {
-          if (this.beep_sound) {
-            let s = this.beep_sound.clone();
-
-            s.on(
-              "end",
-              function () {
-                s._events.end.pop();
-                s.disconnect();
-                s = null;
-              }.bind(this)
-            );
-            s.play();
-            s.sourceNode.playbackRate.value = gsap.utils.random(0.9, 1.5);
-          }
-          let lines = this.mother_container_elm.querySelectorAll(".line, .boot_screen");
-          lines.forEach(
-            function (l) {
-              gsap.set(l, { display: "none" });
-            }.bind(this)
-          );
-        }.bind(this)
-      )
-      .set(this.root_elm, { opacity: 1 })
-      .set([this.top_elm, this.bottom_elm], { opacity: 1, stagger: 0.05 })
-      .set([this.top_elm, this.bottom_elm], {
-        opacity: 0,
-        stagger: 0.05,
-        delay: 0.1
-      });
-    return tl;
-  } //animate
+   //animate
 } //ClearScreen
 
 
@@ -252,70 +125,34 @@ class BootScreen {
   build() {
     this.root_elm = document.createElement("div");
     this.root_elm.classList.add("boot_screen");
-    gsap.set(this.root_elm,{
-      display:'grid',
-      gridTemplateRows:'1fr',
-      gridTemplateColumns:'1fr',
-      alignItems:'center',
-      justifyItems:'center'
-    })
 
     this.random_chars_screen = document.createElement("div");
     this.random_chars_screen.classList.add("random_chars_screen");
-    gsap.set(this.random_chars_screen,{
-      width:'75vw',
-      height:'50vh',
-      position:'relative',
-      overflow:'hidden',
-      gridRow:1,
-      gridColumn:1
-    })
  
     this.root_elm.appendChild(this.random_chars_screen);
 
-    gsap.set(this.root_elm, {
-      position: "absolute",
-      left: 0,
-      top: 0,
-      width: "100%",
-      height: "100%"
-    });
     this.line_elms = [];
     this.line_section_elms = [];
     for (var i = 0; i < this.num_lines; i++) {
       let line_elm = document.createElement("div");
       line_elm.classList.add("line_elm");
       line_elm.classList.add(`line_elm_${i}`);
-      gsap.set(line_elm, { display: "grid", gridTemplateRows: "1fr" });
+      
       for (var n = 0; n < this.line_sections; n++) {
         let sec = document.createElement("div");
-        gsap.set(sec,{
-          display:'inline-block',
-          opacity:0,
-          gridRow:1,
-          fontSize:'2vh',
-          justifySelf:gsap.utils.random(['start','end','center'])
-        });
         sec.classList.add("section");
-        let char = gsap.utils.random(this.chars);
         sec.innerHTML = `<span class='char'>${char}</span><span class='char'>${char}</span><span class='char'>${char}</span><span class='char'>${char}</span><span class='char'>${char}</span><span class='char'>${char}</span><span class='char'>${char}</span><span class='char'>${char}</span>`;
         line_elm.appendChild(sec);
-        gsap.set(sec.querySelectorAll('.char'),{opacity:0});
-        gsap.set([...sec.querySelectorAll('.char')].slice(0, gsap.utils.random(2,7)),{opacity:1});
         this.line_section_elms.push(sec);
       } //for line section
       this.random_chars_screen.appendChild(line_elm);
       this.line_elms.push(line_elm);
     } //for line nums
-    for(var ls = 0; ls < (this.line_section_elms.length/3); ls++){
-      let lse = gsap.utils.random(this.line_section_elms);
-      gsap.set(lse,{opacity:1});
-    }//for
-    
+
+
     //front bars
     this.bars_container = document.createElement('div');
     this.bars_container.classList.add('bars_container');
-    gsap.set(this.bars_container,{position:'absolute', left:0, top:0, width:'100%', height:'100%'})
     this.random_chars_screen.appendChild(this.bars_container);
     
     this.bars_elm = [];
@@ -327,25 +164,7 @@ class BootScreen {
       let bar_glow = document.createElement('div');
       bar_glow.classList.add('bar_glow');
       bar_elm.appendChild(bar_glow);
-      
-      gsap.set(bar_elm,{
-        background:'#80ff10',
-        '--glow-opacity':0,
-        width:"random(15,100)%",
-        height:"random(1,2)%",
-        position:'absolute',
-        top:"random(0,100)%",
-        left:"random(-25,75)%",
-        opacity:0
-      });
-      
-      gsap.set(bar_glow,{
-        width:'100%',
-        height:'100%',
-        boxShadow:'0px 0px 5px #80ff10, 0px 0px 15px #80ff10, 0px 0px 20px #80ff10',
-        opacity:'var(--glow-opacity)'
-      })
-      
+          
       this.bars_elm.push(bar_elm);
     }//for this.num_bars
     //end front bars
@@ -353,40 +172,19 @@ class BootScreen {
     //detail screen
     this.details_screen = document.createElement('div');
     this.details_screen.classList.add('details_screen');
-    gsap.set(this.details_screen,{
-      display: 'grid',
-      gridTemplateRows: '1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr',
-      gridTemplateColumns: '1fr',
-      gridRow: 1,
-      gridColumn: 1,
-      width:'75vw',
-      height:'50vh',
-      textTransform:'uppercase',
-      fontSize:'1.1vh'
-    })
-    
     
     this.details_screen_data.forEach(function(detail_line){
       let d_line = document.createElement('div');
       d_line.classList.add('detail_line');
       
-      gsap.set(d_line,{'--text-shadow-opacity': 0});
-       
-      // let shadow_string = '0px 0px 5px rgba(50,100,6,var(--text-shadow-opacity))';
-      // for(var i = 0; i < 18; i++){
-      //   shadow_string += `, 0px 0px ${5 * (i+1)}px rgba(50,100,6,var(--text-shadow-opacity))`
-      // }//for
-      // gsap.set(d_line,{textShadow: shadow_string});
-      
-      gsap.set(d_line, {fontSize:'2.2vh',display:'grid', gridGap:'1vw', gridTemplateColumns:`1fr 1fr 1fr 1fr`,  alignItems:'center'})
+    
+
       detail_line.forEach(function(line_data,index){//cols
         let copy = line_data.copy;
         let detail_copy_elm = document.createElement('div');
         detail_copy_elm.classList.add('detail_copy');
         detail_copy_elm.innerHTML = copy;
-        if(line_data.col_span){
-          gsap.set(detail_copy_elm,{gridColumn:`${index+1} / span ${line_data.col_span}`})
-        }
+
         d_line.appendChild(detail_copy_elm);
         
       }.bind(this))
@@ -401,7 +199,6 @@ class BootScreen {
   } //build
   
   detailsScreenAnimation(){
-     let tl = gsap.timeline();
      this.details_screen.querySelectorAll('.detail_line').forEach(function(line_elm,index){
       tl.set(line_elm,{'--text-shadow-opacity':0});
       tl.fromTo(line_elm.querySelectorAll('.char'),{
@@ -412,13 +209,6 @@ class BootScreen {
         duration:0.01,
         onComplete:function(){
           
-          gsap.fromTo(line_elm,
-            {'--text-shadow-opacity':1},
-            {
-              '--text-shadow-opacity':0,
-              duration:1,
-              ease:'expo.out'
-            })
           
             if (this.detail_beep_sound) {
               let s = this.detail_beep_sound.clone();
@@ -432,7 +222,6 @@ class BootScreen {
                 }.bind(this)
               );
               s.play();
-              s.sourceNode.playbackRate.value = gsap.utils.random(0.9, 1.5);
             }
         }.bind(this)
       });// tl.fromTo
@@ -445,59 +234,7 @@ class BootScreen {
     return tl;
   }//detailsScreenAnimation
   
-  
-  barsAnimation(){
-    this.bar_tl = gsap.timeline({repeatRefresh:true})
-    
-    this.bars_elm.forEach(function(bar_elm){
-      let b_tl = gsap.timeline({repeatRefresh:true,repeat:-1})
-      b_tl.set(bar_elm,{
-        width:"random(15,100)%",
-        height:"random(1,2)%",
-        top:"random(0,100)%",
-        left:"random(-25,75)%",
-      })
-      b_tl.to(bar_elm,{opacity:1,'--glow-opacity':1,delay:"random(0,2.5)",duration:0.05,ease:'expo.out'})
-      b_tl.to(bar_elm,{opacity:0, '--glow-opacity':0,duration:"random(0.2,0.5)"})
-      this.bar_tl.add(b_tl,0)
-    }.bind(this));//this.bars_elm.forEach
-    
-    return this.bar_tl;
-  }//barsAnimation
-  
-  charsBGAnimation(){
-    this.chars_tl =  gsap.timeline({repeat:-1,repeatRefresh:true})
-    .set(this.line_section_elms,{opacity:0,delay:0.1})
-    .add(function(){
-      for(var ls = 0; ls < (this.line_section_elms.length/gsap.utils.random(3,4,1)); ls++){
-        let lse = gsap.utils.random(this.line_section_elms);
-        gsap.set(lse,{
-          opacity: 1,
-          justifySelf: gsap.utils.random(['start','end','center'])
-        });
-      }//for
-    }.bind(this));
-    return this.chars_tl;
-  }//charsBGAnimation
-  
- 
-  animate(){
-    let tl = gsap.timeline()
-    .add(function(){
-      this.boot_sound.play();
-      gsap.set(this.root_elm,{display:'grid'})
-      gsap.set(this.bars_container,{opacity:1})
-      this.charsBGAnimation();
-      this.barsAnimation();
-    }.bind(this))
-    .add(this.detailsScreenAnimation(),2)
-    .add(function(){
-      this.chars_tl.pause();
-      this.bar_tl.pause();
-      gsap.set(this.bars_container,{opacity:0})
-    }.bind(this),'-=0.15')
-    return tl;
-  }//animate
+
   
 } //BootScreen
 
@@ -517,7 +254,6 @@ class Mother {
     this.detail_beep_src = opts.detail_beep_src || "https://assets.codepen.io/927237/detail_beep.mp3";
     this.lines = [];
     this.cmds = [];
-    this.master_tl = gsap.timeline({ paused: true, repeat: -1 });
     this.build();
   }
 
@@ -525,28 +261,19 @@ class Mother {
     this.start_button = document.createElement('div');
     this.start_button.classList.add('start_button');
     this.start_button.innerHTML = '<span>BOOT</span>';
-    gsap.set(this.start_button,{
-      border:'1px solid #80ff10',
-      display:'grid',
-      alignItems:'center',
-      justifyItems:'center',
-      fontSize:'3.5vh',
-      padding:'15px',
-      boxSizing:'border-box',
-      cursor:'pointer'
-    });
+    
     this.mother_container_elm.appendChild(this.start_button);
     
     this.start_button.addEventListener('click',function(e){
-      gsap.set(this.start_button,{display:'none'})
+      
       this.loadAndPlay();
     }.bind(this));
     
     this.start_button.addEventListener('mouseover',function(e){
-      gsap.set(this.start_button,{border:'1px solid transparent', color:'#000',background:'#80ff10'})
+      
     }.bind(this))
     this.start_button.addEventListener('mouseout',function(e){
-      gsap.set(this.start_button,{border:'1px solid #80ff10', color:'#80ff10',background:'transparent'})
+      
     }.bind(this))
   }//build
   
@@ -633,13 +360,7 @@ class Mother {
             break;
           case "delay":
             c = {
-              animate: function () {
-                return gsap.fromTo(
-                  { v: 0 },
-                  { v: 0 },
-                  { v: 1, duration: cmd.t || 1 }
-                );
-              }
+              
             };
             break;
           default:
